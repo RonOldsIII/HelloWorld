876b1e5ff551ce0469bd3af49fb3fdd2091d46be
RonOldsIII <ronaldoldsiii@rmoiiis-MacBook-Pro-2.local>
2014-08-12 10:10:34 -0400

    First checkin

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
                             
+    @IBOutlet weak var displayLabel: UILabel!
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonclicked(sender: UIButton) {
+        counter++
+        displayLabel.text = "Clicked \(counter) time(s)"
+        
+    }
 
+    @IBAction func clearCount(sender: UIButton) {
+        counter = 0
+        displayLabel.text = "Clicked \(counter) time(s)"
+    }
 }
 
